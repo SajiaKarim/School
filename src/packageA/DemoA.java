3b0dee4accdf78811a4e0d8e2125ce81a7120fa8
sajia <sajia@10.0.0.77>
2021-11-27 15:07:07 -0500

    I add a method

--- src/packageA/DemoA.java
+++ src/packageA/DemoA.java
@@ -4,5 +4,9 @@ public class DemoA {
 	public static void main(String[] args) {
 		System.out.println("Secound practice for GitHup");
 	}
+	
+	public static void addNum(int a, int b) {
+		System.out.println(a+b);
+	}
 
 }
